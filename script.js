a12e60a81555162f4d19c858763a777cb0979488
iara <iara.freitas.job@gmail.com>
2024-06-07 20:31:05 -0300

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeImage() {
+    const img = document.getElementById('myImage');
+    const currentSrc = img.getAttribute('src');
+
+    if(currentSrc ==='1.jpg') {
+        img.setAttribute('src', '2.jpg');
+        img.setAttribute('alt', 'foto2');
+    } else {
+        img.setAttribute('src', '3.jpg');
+        img.setAttribute('src', 'foto3')
+    }
+
+
+}
