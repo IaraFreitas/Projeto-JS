53c2a68be89951fb6cb1546ada2a8bd986e4c18f
iara <iara.freitas.job@gmail.com>
2024-06-07 21:24:15 -0300

    update

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-function changeImage1() {
+// function changeImage1() {
     
-    const img = document.getElementById('myImage1');
+//     const img = document.getElementById('myImage1');
    
-    const currentSrc = img.getAttribute('src');
+//     const currentSrc = img.getAttribute('src');
 
-    if (currentSrc === '1.jpg') {
-        img.setAttribute('src', 'PROJETO-JS/2.jpg');
-        img.setAttribute('alt', 'foto2');
-    } else {
-        img.setAttribute('src', 'PROJETO-JS/1.jpg');
-        img.setAttribute('src', 'foto1');
+//     if (currentSrc === '1.jpg') {
+//         img.setAttribute('src', 'PROJETO-JS/2.jpg');
+//         img.setAttribute('alt', 'foto2');
+//     } else {
+//         img.setAttribute('src', 'PROJETO-JS/1.jpg');
+//         img.setAttribute('src', 'foto1');
+//     }
+
+// }
+
+function changeImageDois() {
+
+    const img = document.getElementById ("testeDois");
+
+    const testeSrc = img.getAttribute('src');
+
+    if (testeSrc === '1.jpg') {
+        img.
     }
-
-
 }
