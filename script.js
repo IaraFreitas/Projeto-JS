0afaf73fc6028427b088b12491e6cad0f639ffc5
@if.dev_ <iara.freitas.job@gmail.com>
2024-06-08 13:51:04 -0300

    update

--- script.js
+++ script.js
@@ -17,3 +17,40 @@ function changeImageMuseum() {
 }
 
 
+function changeImageLugares() {
+
+    const img = document.getElementById('lugares');
+
+    const lugaresSrc = img.getAttribute('src');
+
+    if (lugaresSrc === 'lugares-1-LuzStation.jpg') {
+        img.setAttribute('src', 'lugares-2-masp.jpg');
+        img.setAttribute('alt', 'lugaresTwo');
+    }
+
+    else {
+        img.setAttribute('src', 'lugares-3-MercadaoSP.jpg');
+        img.setAttribute('alt', 'lugaresThree');
+    }
+
+    
+}
+
+function changeImageCtoCultural() {
+
+    const img = document.getElementById('cto-cultural');
+
+    const ctoCulturalSrc = img.getAttribute('src');
+
+    if (ctoCulturalSrc === 'cto-cultural-1-CCBB.jpg') {
+        img.setAttribute('src', 'cto-Cultural-2-Vergueiro.jpg');
+        img.setAttribute('alt', 'ctoCulturalTwo');
+    }
+
+    else {
+        img.setAttribute('src', 'cto-cultural-3-sala-sp.jpg');
+        img.setAttribute('alt', 'ctoCulturalThree');
+    }
+    
+}
+
