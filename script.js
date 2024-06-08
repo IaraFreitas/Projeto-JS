746e4e4a67ca825073224588d18be18079609a97
iara <iara.freitas.job@gmail.com>
2024-06-07 21:12:48 -0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function changeImage1() {
+    
     const img = document.getElementById('myImage1');
+   
     const currentSrc = img.getAttribute('src');
 
-    if(currentSrc === '1.jpg') {
+    if (currentSrc === '1.jpg') {
         img.setAttribute('src', 'PROJETO-JS/2.jpg');
-        img.setAttribute('alt', 'foto1');
+        img.setAttribute('alt', 'foto2');
     } else {
         img.setAttribute('src', 'PROJETO-JS/1.jpg');
-        img.setAttribute('src', 'foto2');
+        img.setAttribute('src', 'foto1');
     }
 
 
